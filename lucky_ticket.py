bd3fd19fdb5b24198d369d65bcb646fc485b1b41
Xiloid <faustov.roman.a@gmail.com>
2021-02-17 21:29:50 +0200

    commited

--- lucky_ticket.py
+++ lucky_ticket.py
@@ -5,7 +5,15 @@
 
 
 def is_lucky(ticket_num):
-    pass
+    s = str(ticket_num)
+    x = list(s)
+    print(x)
+    sum1 = int(s[0]) + int(s[1]) + int(s[2])
+    sum2 = int(s[3]) + int(s[4]) + int(s[5])
+    if sum1 == sum2:
+        print('Счастливый')
+    else:
+        print('Обычный')
 
 
 assert is_lucky(1230) is True
