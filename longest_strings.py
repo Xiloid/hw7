97f13c4833bf6e7bd5bfbafc7fcb2e5e4c97b615
Xiloid <faustov.roman.a@gmail.com>
2021-02-17 21:08:58 +0200

    commited

--- longest_strings.py
+++ longest_strings.py
@@ -7,24 +7,23 @@
 """
 
 """ Функция возвращает список самых длинных строк из списка list_in """
+
+
 def longest_strings(list_in):
-#    t_1 = ["x"]
-#    max(t_1, key=len)
-#    return t_1
-    t_2 = ["abc", "eeee", "abcd", "dcd"]
-#    length = max(t_2, key=len)
-#    print(length)
-    str_list = [x for x in t_2 if isinstance(x, str)]
-    longestword = sorted(str_list, key=len, reverse=True)[0]
-    print(longestword)
+    longest = []
+    y = max(list_in, key=len)
+    for i in list_in:
+        if len(i) == len(y):
+            longest.append(i)
+    return longest
 
 
-#t_1 = ["x"]
-#assert longest_strings(t_1) == ["x"]
+t_1 = ["x"]
+assert longest_strings(t_1) == ["x"]
 
 t_2 = ["abc", "eeee", "abcd", "dcd"]
 assert longest_strings(t_2) == ["eeee", "abcd"]
-'''
+
 t_3 = ["a", "abc", "cbd", "zzzzzz", "a", "abcdef", "asasa", "aaaaaa"]
 assert longest_strings(t_3) == ["zzzzzz", "abcdef", "aaaaaa"]
 
@@ -33,5 +32,5 @@ assert longest_strings(t_4) == ["varennyky"]
 
 t_5 = ["abacaba", "abacab", "abac", "xxxxxx"]
 assert longest_strings(t_5) == ["abacaba"]
-'''
+
 print("All tests passed successfully!")
